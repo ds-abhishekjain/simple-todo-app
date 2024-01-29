23f44fa7a7e3b0f4e26dba71a0ca0b972f1d20d5
ds-abhishekjain <ds.abhishekjain@gmail.com>
2024-01-29 16:03:16 +0530

    Backend

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://admin:eS3kgx7Y9j7jRNrC@jsprojects.kzmeu5h.mongodb.net/');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const mongo_todo_schema = new mongoose.Schema({
+    title : String,
+    description : String,
+    completed : Boolean
+})
+
+const todoModel = mongoose.model('todoModel',mongo_todo_schema)
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,58 @@
 const express = require('express')
 const port = 3000
 const app = express()
+const {todoModel} = require('./db')
 
 app.use(express.json())
 
-app.get('/todos',(req,res) => {
-    res.send("Hello!")
+const {todoSchema,updateTodoSchema} = require("./types");
+
+
+app.get('/todos',async (req,res) => {
+    const todos = await todoModel.find({}); 
+    res.json({
+        todos
+    })
 })
 
-app.post('/todos',(req,res) => {
+app.post('/todos',async (req,res) => {
+    const createPayLoad = req.body;
+    const parsedPayLoad = todoSchema.safeParse(createPayLoad)
+    if (!parsedPayLoad.success) {
+        res.status(411).json({
+            msg : "You sent wrong inputs"
+        })
+        return;
+    }
+    // put it in mongo db
+    await todoModel.create({
+        title : createPayLoad.title,
+        description : createPayLoad.description,
+        completed : false
+    }) 
 
+    res.json({
+        msg: "Todo created"
+    })
 })
 
-app.put('/todos',(req,res) => {
-
+app.put('/completed',async (req,res) => {
+    const updatePayLoad = req.body;
+    const parsedPayLoad = todoSchema.safeParse(updatePayLoad)
+    if (!parsedPayLoad.success) {
+        res.status(411).json({
+            msg : "You sent wrong inputs"
+        })
+        return;
+    }
+    await todoModel.update({
+        _id: req.body.id
+    },{
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed"
+    })
 })
 
 app.listen(port,() => {
